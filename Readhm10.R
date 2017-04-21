bbf36f25c318219416827a88799aa4ef80287805
Kris-Kindle <Kristopher1@mail.usf.edu>
2017-04-20 21:36:19 -0400

    Fixed iteration in Readhm10

--- Readhm10.R
+++ Readhm10.R
@@ -1,46 +1,30 @@
-read_hm10 <- function(file){
+
+setwd("/Users/Kris/Google Drive/Thesis/Data/CenterLineMiles")
+
+read_hm20 <- function(file){
   file <- read.csv(file, header = TRUE, as.is = T)
 }
 
-clean_hm10 <- function(dataframe){
-  dataframe <<- dataframe[-1:-7,]
-  names(cmt96) <- c("State", "CMT")
-  dataframe <<- dataframe[,-2:-15]
-  cmt96 <-cmt96[-52:-59,]
+clean_hm20 <- function(dataframe){
+  dataframe <- select(dataframe, 1, 16)
+  dataframe <- dataframe[-1:-7,]
+  names(dataframe) <- c("State", "CMT")
+  dataframe <- dataframe[1:51,]
+  dataframe
 }
 
 listfiles <- list.files(getwd())
 listfiles <- listfiles[-20]
 
-cmtlist <- lapply(listfiles, read_hm10)
-cmtlist <- lapply(cmtlist, clean_hm10)
+cmtlist <- lapply(listfiles, read_hm20)
+cmtlist <- lapply(cmtlist, clean_hm20)
 
-number <- seq(1,19,1)
+cmt <- do.call("rbind", cmtlist)
 
-df96 <- as.data.frame(cmtlist[1])
-df97 <- as.data.frame(cmtlist[2])
-df98 <- as.data.frame(cmtlist[3])
-df99 <- as.data.frame(cmtlist[4])
-df00 <- as.data.frame(cmtlist[5])
-df01 <- as.data.frame(cmtlist[6])
-df02 <- as.data.frame(cmtlist[7])
-df03 <- as.data.frame(cmtlist[8])
-df04 <- as.data.frame(cmtlist[9])
-df05 <- as.data.frame(cmtlist[10])
-df06 <- as.data.frame(cmtlist[11])
-df07 <- as.data.frame(cmtlist[12])
-df08 <- as.data.frame(cmtlist[13])
-df09 <- as.data.frame(cmtlist[14])
-df10 <- as.data.frame(cmtlist[15])
-df11 <- as.data.frame(cmtlist[16])
-df12 <- as.data.frame(cmtlist[17])
-df13 <- as.data.frame(cmtlist[18])
-df14 <- as.data.frame(cmtlist[19])
-
-cmt <- rbind(df96,df97,df98,df99,df00,df01,df02,df03,df04,df05,df06,df07,df08,df09,df10,df11,df12,df13,df14, deparse.level = 1)
-
-dataset$CMT <- cmt$CMT
+cmtlist[[1]]
 
 #This will read in as factor variables for some reason
 #Output the final dataset out open in Excel and change the format to number
 #Reload the dataset and it should have numeric or integer data type
+
+
