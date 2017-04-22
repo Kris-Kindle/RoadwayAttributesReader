0d5498ad60fe8e13502cedb089220bde247ba549
Kris-Kindle <Kristopher1@mail.usf.edu>
2017-04-21 21:14:05 -0400

    Fixed HM60 reader and updated README

--- Readhm20.R
+++ Readhm20.R
@@ -1,61 +1,30 @@
-library(tidyverse)
-setwd("/Users/Kris/Google Drive/Thesis/Data/CenterLineMiles")
-HM20_files <- list.files(getwd())
-HM20_files <- HM20_files[-20]
 
+setwd("/Users/Kris/Google Drive/Thesis/Data/CenterLineMiles")
 
 read_hm20 <- function(file){
-  file<- read.csv(file, header = TRUE, stringsAsFactors = F)
+  file <- read.csv(file, header = TRUE, as.is = T)
 }
 
 clean_hm20 <- function(dataframe){
   dataframe <- select(dataframe, 1, 16)
   dataframe <- dataframe[-1:-7,]
-  names(dataframe) <- c("State", "CLM")
+  names(dataframe) <- c("State", "CMT")
   dataframe <- dataframe[1:51,]
   dataframe
 }
 
+listfiles <- list.files(getwd())
+listfiles <- listfiles[-20]
 
-clmlist <- lapply(HM20_files, read_hm20)
+cmtlist <- lapply(listfiles, read_hm20)
+cmtlist <- lapply(cmtlist, clean_hm20)
 
-number <- seq(1,19,1)
+cmt <- do.call("rbind", cmtlist)
 
-df96 <- clean_hm20(as.data.frame(clmlist[1]))
-df97 <- clean_hm20(as.data.frame(clmlist[2]))
-df98 <- clean_hm20(as.data.frame(clmlist[3]))
-df99 <- clean_hm20(as.data.frame(clmlist[4]))
-df00 <- clean_hm20(as.data.frame(clmlist[5]))
-df01 <- clean_hm20(as.data.frame(clmlist[6]))
-df02 <- clean_hm20(as.data.frame(clmlist[7]))
-df03 <- clean_hm20(as.data.frame(clmlist[8]))
-df04 <- clean_hm20(as.data.frame(clmlist[9]))
-df05 <- clean_hm20(as.data.frame(clmlist[10]))
-df06 <- clean_hm20(as.data.frame(clmlist[11]))
-df07 <- clean_hm20(as.data.frame(clmlist[12]))
-df08 <- clean_hm20(as.data.frame(clmlist[13]))
-df09 <- clean_hm20(as.data.frame(clmlist[14]))
-df10 <- clean_hm20(as.data.frame(clmlist[15]))
-df11 <- clean_hm20(as.data.frame(clmlist[16]))
-df12 <- clean_hm20(as.data.frame(clmlist[17]))
-df13 <- clean_hm20(as.data.frame(clmlist[18]))
-df14 <- clean_hm20(as.data.frame(clmlist[19]))
-
-clm <- rbind(df96,df97,df98,df99,df00,df01,df02,df03,df04,df05,
-             df06,df07,df08,df09,df10,df11,df12,df13,df14, 
-             deparse.level = 1)
-
-#This saves the day!!!!
-#Changes charachter variables into numeric
-#The variables are being imported as factor due to the 
-#comma in the number but we forced it into 
-#character then from character to numeric by removing the comma
-clm$CLM <- as.numeric(gsub(",","",clm$CLM))
-
-dataset3$CLM <- clm$CLM
+cmtlist[[1]]
 
 #This will read in as factor variables for some reason
-#Output the final dataset out open in Excel 
-#and change the format to number
-#Reload the dataset and it should have 
-#numeric or integer data type
+#Output the final dataset out open in Excel and change the format to number
+#Reload the dataset and it should have numeric or integer data type
+
+
--- Readhm60.R
+++ Readhm60.R
@@ -9,9 +9,10 @@ read_hm60 <- function(file){
   file <- read.csv(file, header = FALSE,  stringsAsFactors = F)
 }
 
+
 clean_hm60 <- function(dataframe){
   dataframe <- select(dataframe, 1, 16)
-  dataframe <- dataframe[-1:-8,]
+  dataframe <- dataframe[-1:-7,]
   names(dataframe) <- c("State", "LM")
   dataframe <- dataframe[1:51,]
   dataframe
@@ -20,38 +21,17 @@ clean_hm60 <- function(dataframe){
 
 lmlist <- lapply(HM60_files, read_hm60)
 
-number <- seq(1,19,1)
+test <- clean_hm60(lmlist[[13]])
 
-df96 <- clean_hm60(as.data.frame(lmlist[1]))
-df97 <- clean_hm60(as.data.frame(lmlist[2]))
-df98 <- clean_hm60(as.data.frame(lmlist[3]))
-df99 <- clean_hm60(as.data.frame(lmlist[4]))
-df00 <- clean_hm60(as.data.frame(lmlist[5]))
-df01 <- clean_hm60(as.data.frame(lmlist[6]))
-df02 <- clean_hm60(as.data.frame(lmlist[7]))
-df03 <- clean_hm60(as.data.frame(lmlist[8]))
-df04 <- clean_hm60(as.data.frame(lmlist[9]))
-df05 <- clean_hm60(as.data.frame(lmlist[10]))
-df06 <- clean_hm60(as.data.frame(lmlist[11]))
-df07 <- clean_hm60(as.data.frame(lmlist[12]))
-df08 <- clean_hm60(as.data.frame(lmlist[13]))
-df09 <- clean_hm60(as.data.frame(lmlist[14]))
-df10 <- clean_hm60(as.data.frame(lmlist[15]))
-df11 <- clean_hm60(as.data.frame(lmlist[16]))
-df12 <- clean_hm60(as.data.frame(lmlist[17]))
-df13 <- clean_hm60(as.data.frame(lmlist[18]))
-df14 <- clean_hm60(as.data.frame(lmlist[19]))
+lmlist <- lapply(lmlist, clean_hm60)
 
-lm <- rbind(df96,df97,df98,df99,df00,df01,df02,df03,df04,df05,
-            df06,df07,df08,df09,df10,df11,df12,df13,df14, 
-            deparse.level = 1)
+lm <- do.call("rbind", lmlist)
+
 
 lm$LM <- as.numeric(gsub(",","",lm$LM))
 
 dataset3$LM <- lm$LM
 
 #This will read in as factor variables for some reason
-#Output the final dataset out open in Excel 
-#and change the format to number
-#Reload the dataset and it should 
-#have numeric or integer data type
+#Output the final dataset out open in Excel and change the format to number
+#Reload the dataset and it should have numeric or integer data type
